90e622c3c222d51679c0e77a00f8c907c905eb02
fifi98 <bel.filip@icloud.com>
2022-04-02 22:40:13 +0200

    feat: view expense sheet

--- src/features/expenses/components/ListItem.tsx
+++ src/features/expenses/components/ListItem.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, ViewStyle } from 'react-native';
 
 import { Space } from '../../../common/components';
 
 import colors from '../../../constants/colors';
 import { fontSize } from '../../../constants/typography';
 
-interface ListItemProps {}
+interface ListItemProps {
+  containerStyle?: ViewStyle;
+}
 
-const ListItem: React.FunctionComponent<ListItemProps> = () => {
+const ListItem: React.FunctionComponent<ListItemProps> = ({ containerStyle }) => {
   return (
     <View
-      style={{
-        flexDirection: 'row',
-        backgroundColor: colors.white,
-        borderRadius: 20,
-        paddingVertical: 15,
-        paddingHorizontal: 22,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom: 12,
-      }}>
+      style={[
+        {
+          flexDirection: 'row',
+          backgroundColor: colors.white,
+          borderRadius: 20,
+          paddingVertical: 15,
+          paddingHorizontal: 22,
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          marginBottom: 12,
+        },
+        containerStyle,
+      ]}>
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
         <Image source={require('../../shopping-list/assets/mleko.png')} style={{ width: 50, height: 50, marginRight: 18 }} />
         <View>
--- src/features/expenses/components/SheetContent.tsx
+++ src/features/expenses/components/SheetContent.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import { ActionButton, Divider } from '../../../common/components';
+import colors from '../../../constants/colors';
+import { fontSize } from '../../../constants/typography';
+import { PencilIcon } from '../../dashboard/assets';
+import ListItem from './ListItem';
 
 interface SheetContentProps {}
 
 const SheetContent: React.FunctionComponent<SheetContentProps> = () => {
   return (
-    <View>
-      <Text>167 HRK</Text>
+    <View style={{ paddingHorizontal: 24, marginTop: 30, flex: 1 }}>
+      <View style={{ marginBottom: 30 }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Text style={{ fontSize: fontSize.large, fontWeight: 'bold' }}>167 HRK</Text>
+          <ActionButton icon={PencilIcon} />
+        </View>
+        <Text style={{ fontSize: fontSize.small, color: colors.gray }}>2. travnja u 14:57</Text>
+      </View>
+      <FlatList
+        data={[
+          {
+            id: '1',
+            name: 'Nekaj',
+          },
+          {
+            id: '2',
+            name: 'Nekaj',
+          },
+          {
+            id: '3',
+            name: 'Nekaj',
+          },
+        ]}
+        renderItem={({ item }) => <ListItem containerStyle={{ paddingHorizontal: 0, marginBottom: 0, paddingVertical: 8 }} />}
+        ItemSeparatorComponent={() => <Divider />}
+      />
     </View>
   );
 };
